502cae45931f724f11483767d61bafeba277d2d9
Perillos <valero.ce@gmail.com>
2024-01-31 07:12:44 +0100

    Add form for creating an account

--- views/auth/crear-cuenta.php
+++ views/auth/crear-cuenta.php
@@ -1,1 +1,9 @@
 <h1 class="nombre-pagina">Crear Cuenta</h1>
+<p class="descripcion-pagina">Rellena el siguiente formulario para crear una cuenta</p>
+
+<form class="formulario" method="POST" action="/crear-cuenta">
+    <div class="campo">
+        <label for="nombre">Nombre</label>
+        <input type="text" id="nombre" name="nombre" placeholder="Tu Nombre" />
+    </div>
+</form>
