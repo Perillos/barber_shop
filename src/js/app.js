94d75ea855c04df5346e6cf423ecffbd8800705b
Perillos <valero.ce@gmail.com>
2024-02-19 07:03:46 +0100

    Agrega funcionalidad de paginación a la aplicación

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 let paso = 1;
+const pasoInicial = 1;
+const pasoFinal = 3;
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   iniciarApp();
 });
 
@@ -11,36 +13,39 @@ function iniciarApp() {
   tabs();
   // Agrega o quita los bontones del paginador
   botonesPaginador();
+
+  paginaAnterior();
+  paginaSiguiente();
 }
 
 function mostrarSeccion() {
   // Oculatar la sección que tenga la clase de mostrar
-  const seccionAnterior = document.querySelector(".mostrar");
+  const seccionAnterior = document.querySelector('.mostrar');
   if (seccionAnterior) {
-    seccionAnterior.classList.remove("mostrar");
+    seccionAnterior.classList.remove('mostrar');
   }
 
   // Seleccionar la sección con el paso
   const pasoSelector = `#paso-${paso}`;
   const seccion = document.querySelector(pasoSelector);
-  seccion.classList.add("mostrar");
+  seccion.classList.add('mostrar');
 
   // Quita la clase de actual al tab anterior
-  const tabAnterior = document.querySelector(".actual");
+  const tabAnterior = document.querySelector('.actual');
   if (tabAnterior) {
-    tabAnterior.classList.remove("actual");
+    tabAnterior.classList.remove('actual');
   }
 
   // Resalta el tab actual
   const tab = document.querySelector(`[data-paso="${paso}"]`);
-  tab.classList.add("actual");
+  tab.classList.add('actual');
 }
 
 function tabs() {
-  const botones = document.querySelectorAll(".tabs button");
+  const botones = document.querySelectorAll('.tabs button');
 
   botones.forEach((boton) => {
-    boton.addEventListener("click", function (eve) {
+    boton.addEventListener('click', function (eve) {
       paso = parseInt(eve.target.dataset.paso);
 
       mostrarSeccion();
@@ -50,17 +55,38 @@ function tabs() {
 }
 
 function botonesPaginador() {
-  const paginaAnterior = document.querySelector("#anterior");
-  const paginaSiguiente = document.querySelector("#siguiente");
+  const paginaAnterior = document.querySelector('#anterior');
+  const paginaSiguiente = document.querySelector('#siguiente');
 
   if (paso === 1) {
-    paginaAnterior.classList.add("ocultar");
-    paginaSiguiente.classList.remove("ocultar");
+    paginaAnterior.classList.add('ocultar');
+    paginaSiguiente.classList.remove('ocultar');
   } else if (paso === 3) {
-    paginaAnterior.classList.remove("ocultar");
-    paginaSiguiente.classList.add("ocultar");
+    paginaAnterior.classList.remove('ocultar');
+    paginaSiguiente.classList.add('ocultar');
   } else {
-    paginaAnterior.classList.remove("ocultar");
-    paginaSiguiente.classList.add("ocultar");
+    paginaAnterior.classList.remove('ocultar');
+    paginaSiguiente.classList.remove('ocultar');
   }
+
+  mostrarSeccion();
+}
+
+function paginaAnterior() {
+  const paginaAnterior = document.querySelector('#anterior');
+  paginaAnterior.addEventListener('click', function () {
+    if (paso <= pasoInicial) return;
+    paso--;
+
+    botonesPaginador();
+  });
+}
+function paginaSiguiente() {
+  const paginaSiguiente = document.querySelector('#siguiente');
+  paginaSiguiente.addEventListener('click', function () {
+    if (paso >= pasoFinal) return;
+    paso++;
+
+    botonesPaginador();
+  });
 }
