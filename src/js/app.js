ae2c35b4cd8f3e374cc9c1d26c1c8f8226db137d
fvalero <fvalero@gesio.com>
2024-02-20 11:36:10 +0100

    Agrega archivo _servicios.scss y objeto cita

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,13 @@ let paso = 1;
 const pasoInicial = 1;
 const pasoFinal = 3;
 
+const cita = {
+  nombre: '',
+  fecha: '',
+  hora: '',
+  servicios: [],
+};
+
 document.addEventListener('DOMContentLoaded', function () {
   iniciarApp();
 });
