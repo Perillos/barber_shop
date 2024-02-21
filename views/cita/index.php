96f81fc805907619d9814062ab73d7895adb164e
fvalero <fvalero@gesio.com>
2024-02-21 15:29:17 +0100

    Actualizar límite mínimo de fecha en campo de fecha

--- views/cita/index.php
+++ views/cita/index.php
@@ -23,7 +23,7 @@
             </div>
             <div class="campo">
                 <label for="fecha">Fecha</label>
-                <input id="fecha" type="date" />
+                <input id="fecha" type="date" min="<?php echo date('Y-m-d', strtotime('+1 day')) ?>" />
             </div>
             <div class="campo">
                 <label for="hora">Hora</label>
